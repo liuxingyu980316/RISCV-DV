2015cf6cfdd32c782029a220d9997c66eb8485d0
liuxingyu980316 <45873408+liuxingyu980316@users.noreply.github.com>
2023-10-18 14:32:12 +0800

    Update riscv_instr_test.sv

--- test/riscv_instr_test.sv
+++ test/riscv_instr_test.sv
@@ -27,27 +27,27 @@ class riscv_instr_test extends riscv_instr_base_test;
     string test_name = $sformatf("%0s_0.S", asm_file_name);
     fd = $fopen(test_name,"w");
     `uvm_info(`gfn, "Creating instruction list", UVM_LOW)
-    riscv_instr::create_instr_list(cfg);
-    riscv_csr_instr::create_csr_filter(cfg);
+    riscv_instr::create_instr_list(cfg);                                            //  形成可产生的指令列表（哪些指令能被随机到）
+    riscv_csr_instr::create_csr_filter(cfg);                                        //  形成csr列表（哪些csr能被随机到）
     `uvm_info(`gfn, "Randomizing instruction list now...", UVM_LOW)
     repeat (10000) begin
-      instr = riscv_instr::get_rand_instr();
-      `DV_CHECK_RANDOMIZE_FATAL(instr);
-      $fwrite(fd, {instr.convert2asm(),"\n"});
+      instr = riscv_instr::get_rand_instr();                                        //  产生一个随机指令
+      `DV_CHECK_RANDOMIZE_FATAL(instr);                                             //  利用instr的pre post randomize 和 constrain 生成：rs rd imm 
+      $fwrite(fd, {instr.convert2asm(),"\n"});                                       
     end
     repeat (10000) begin
-      instr = riscv_instr::get_rand_instr(.include_category({LOAD, STORE}));
-      `DV_CHECK_RANDOMIZE_FATAL(instr);
-      $fwrite(fd, {instr.convert2asm(),"\n"});
-    end
+      instr = riscv_instr::get_rand_instr(.include_category({LOAD, STORE}));        //  传参，产生一个LOAD / STORE 随机指令
+      `DV_CHECK_RANDOMIZE_FATAL(instr);                                             //  利用instr的pre post randomize 和 constrain 生成：rs rd imm 
+      $fwrite(fd, {instr.convert2asm(),"\n"});                                      //  转换成汇编指令写入文件 
+    end 
     repeat (10000) begin
-      instr = riscv_instr::get_rand_instr(.exclude_category({LOAD, STORE , BRANCH}),
+      instr = riscv_instr::get_rand_instr(.exclude_category({LOAD, STORE , BRANCH}),//  传参，产生一个LOAD / STORE / BRANCH 随机指令  group是 RV32I / RV32M
                                           .include_group({RV32I, RV32M}));
-      `DV_CHECK_RANDOMIZE_FATAL(instr);
-      $fwrite(fd, {instr.convert2asm(),"\n"});
+      `DV_CHECK_RANDOMIZE_FATAL(instr);                                             //  利用instr的pre post randomize 和 constrain 生成：rs rd imm 
+      $fwrite(fd, {instr.convert2asm(),"\n"});                                      //  转换成汇编指令写入文件 
     end
-    $fclose(fd);
-    `uvm_info(get_full_name(), $sformatf("%0s is generated", test_name), UVM_LOW)
+    $fclose(fd);                                                                    //  关闭文件
+    `uvm_info(get_full_name(), $sformatf("%0s is generated", test_name), UVM_LOW)   //  打印消息 
   endtask
 
   virtual function void randomize_cfg();                //  和base中的是一样的，不太清楚为什么在extend之后再写一次。 对cfg进行随机
